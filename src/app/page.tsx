9f73cd11659b336a738df58a3cbe0f604b9d98b9
6666 <1327185450@qq.com>
2024-12-09 22:28:04 +0800

    配置nextAuth的credentials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { auth } from '@/auth';
 import { protectServer } from '@/features/auth/utils';
 
 export default async function Home() {
   await protectServer();
 
+  const session = await auth();
   return <div>您已登录</div>;
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,48 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '@/db/';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string | undefined;
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+    }),
+    GitHub,
+    Google,
+  ],
   adapter: DrizzleAdapter(db),
   pages: {
     signIn: '/sign-in',
     // error: '/sign-up',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
   },
 });
